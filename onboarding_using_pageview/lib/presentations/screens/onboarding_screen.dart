10fe2f538447ddb40c3e79e1f4c0607224feef7c
raihan <raihanp0308@gmail.com>
2023-09-01 17:02:26 +0700

    indicator fix

--- onboarding_using_pageview/lib/presentations/screens/onboarding_screen.dart
+++ onboarding_using_pageview/lib/presentations/screens/onboarding_screen.dart
@@ -2,59 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:onboarding_using_pageview/presentations/widgets/splash_widget.dart';
 
 class OnboardingScreen extends StatefulWidget {
-  const OnboardingScreen({super.key});
+  const OnboardingScreen({Key? key}) : super(key: key);
 
   @override
   State<OnboardingScreen> createState() => _OnboardingScreenState();
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  // setting controller
-  final _controller = PageController(
-    initialPage: 0,
-  );
-
-  final List<String> _image = [
-    'assets/images/splash1.png',
-    'assets/images/splash2.png',
-    'assets/images/splash3.png',
-  ];
-
-  final List<String> _title = [
-    'Selamat Datang di Aplikasi Dcoll!',
-    'Kenali Fitur Utama Aplikasi Kami',
-    'Selamat Datang di Aplikasi Dcoll!',
-  ];
-
-  final List<String> _subtitle = [
-    'Pengalaman baru dalam mengatur kunjungan dan peminjaman dengan mudah dan efisien.',
-    'Pengalaman baru dalam mengatur kunjungan dan peminjaman dengan mudah dan efisien.',
-    'Pelajari bagaimana aplikasi kami membantu mengelola aktivitas kunjungan anda.',
-  ];
+  late final PageController controller;
+  double currentPage = 0;
 
   @override
   void initState() {
-    // TODO: implement initState
+    controller = PageController();
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page!;
+      });
+    });
     super.initState();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> image = [
+      'assets/images/splash1.png',
+      'assets/images/splash2.png',
+      'assets/images/splash3.png',
+    ];
+
+    final List<String> title = [
+      'Selamat Datang di Aplikasi Dcoll!',
+      'Kenali Fitur Utama Aplikasi Kami',
+      'Selamat Datang di Aplikasi Dcoll!',
+    ];
+
+    final List<String> subtitle = [
+      'Pengalaman baru dalam mengatur kunjungan dan peminjaman dengan mudah dan efisien.',
+      'Pengalaman baru dalam mengatur kunjungan dan peminjaman dengan mudah dan efisien.',
+      'Pelajari bagaimana aplikasi kami membantu mengelola aktivitas kunjungan anda.',
+    ];
+
+    final onboardWidget = List.generate(image.length, (index) {
+      return SplashWidget(
+        image: image[index],
+        title: title[index],
+        subtitle: subtitle[index],
+      );
+    });
+
     return Scaffold(
       body: PageView.builder(
-        itemCount: _image.length,
+        controller: controller,
+        itemCount: image.length,
         itemBuilder: (context, index) {
-          return SplashWidget(
-            image: _image[index],
-            title: _title[index],
-            subtitle: _subtitle[index],
-          );
+          return onboardWidget[index];
         },
       ),
       bottomNavigationBar: Padding(
@@ -62,36 +70,20 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SizedBox(
-              width: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 8,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: const Color(0xff00636C),
-                    ),
+            Row(
+              children: List.generate(
+                image.length,
+                (index) => Container(
+                  margin: const EdgeInsets.only(right: 6),
+                  width: 8,
+                  height: 8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: currentPage.round() == index
+                        ? const Color(0xff00636C)
+                        : const Color(0xffD9E8E9),
                   ),
-                  Container(
-                    width: 8,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: const Color(0xffD9E8E9),
-                    ),
-                  ),
-                  Container(
-                    width: 8,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: const Color(0xffD9E8E9),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
             Container(
