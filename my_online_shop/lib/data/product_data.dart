ba82a35da4b3cfd67820d6f0f58f7bea66fc7c5d
raihan <raihanp0308@gmail.com>
2023-09-04 14:49:31 +0700

    Setup project and add some packages

---
+++ my_online_shop/lib/data/product_data.dart
@@ -0,0 +1,27 @@
+class ProductData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': '1',
+      'name': 'White Elegan Long Dress',
+      'description': 'A long white dress has an elegant feel, with appropriate patterns and color combinations.',
+      'price': 90.99,
+      'imageUrl':
+          'https://cdn.pixabay.com/photo/2016/04/16/19/51/girl-1333640_1280.jpg',
+    },
+    {
+      'id': '2',
+      'name': 'Wedding Dress',
+      'description': 'Long white wedding dress with elegant nuances, with stitches combined with lace on each side.',
+      'price': 300.99,
+      'imageUrl':
+          'https://cdn.pixabay.com/photo/2016/06/29/08/42/wedding-dress-1486260_1280.jpg',
+    },
+    {
+      'id': '3',
+      'name': 'Japanese Dress',
+      'description': 'Dresses with Japanese culture, made in great detail, with lots of decorations in every part of the dress, so that the wearer looks even more beautiful and elegant.',
+      'price': 150.49,
+      'imageUrl': 'https://cdn.pixabay.com/photo/2018/02/24/20/40/fashion-3179178_1280.jpg',
+    },
+  ];
+}
