6289de88583885b48d9f8937b36af0493e966368
raihan <raihanp0308@gmail.com>
2023-09-12 17:00:39 +0700

    add connection

--- sqlite_example/lib/utils/db.dart
+++ sqlite_example/lib/utils/db.dart
@@ -16,5 +16,14 @@ void main() async {
     // `path` package is best practice to ensure the path is correctly
     // constructed for each platform.
     join(await getDatabasesPath(), 'doggie_database.db'),
+    // When the database is first created, create a table to store dogs.
+    onCreate: (db, version) {
+      // Run the CREATE TABLE statement on the database
+      return db.execute(
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+      );
+    }, // Set the version. This executes the onCreate function and provides a
+    // path to perform database upgrades and downgrades.
+    version: 1,
   );
 }
