53bc3ccb63e11a3df2331a5779e6c26bdbbecbf7
raihan <raihanp0308@gmail.com>
2023-09-14 18:34:50 +0700

    add timer_bloc

--- my_timer_app/lib/timer/bloc/timer_bloc.dart
+++ my_timer_app/lib/timer/bloc/timer_bloc.dart
@@ -1,13 +1,37 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:my_timer_app/ticker.dart';
 
 part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  TimerBloc() : super(TimerInitial()) {
-    on<TimerEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  // define ticker from another file
+  final Ticker _ticker;
+  static const int _duration = 60;
+
+  StreamSubscription<int>? _tickerSubscription;
+
+  // define the initial state of TimerBloc
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+  }
+
+  // method to close timer, then cancel all progress
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  FutureOr<void> _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: event.duration)
+        .listen((duration) => add(_TimerTicked(duration: duration)));
   }
 }
