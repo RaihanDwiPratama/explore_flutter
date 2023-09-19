98ec3973182d4f1ebd060bc2767cd7ef06ff60f9
raihan <raihanp0308@gmail.com>
2023-09-19 16:59:53 +0700

    add bottom navigation bar screens

---
+++ pelatihanku_lxp_apps/lib/presentations/screens/bottom_navigation_bar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Beranda',
+      style: optionStyle,
+    ),
+    Text(
+      'Pelatihan-ku',
+      style: optionStyle,
+    ),
+    Text(
+      'Penugasan',
+      style: optionStyle,
+    ),
+    Text(
+      'Akun',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pelatihan-ku'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.perm_contact_cal),
+            label: 'Pelatihan-ku',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.content_paste),
+            label: 'Penugasan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Akun',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xff3498DB),
+        unselectedItemColor: const Color(0xffCDD5DF),
+        onTap: _onItemTapped,
+        showUnselectedLabels: false,
+      ),
+    );
+  }
+}
