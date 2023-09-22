c6e391d9343921ed4147cf14d1823914400ec0bc
raihan <raihanp0308@gmail.com>
2023-09-22 18:54:09 +0700

    restyle

--- pelatihanku_lxp_apps/lib/presentations/utils/style.dart
+++ pelatihanku_lxp_apps/lib/presentations/utils/style.dart
@@ -10,49 +10,36 @@ class Style {
   static const TextStyle title = TextStyle(
     fontSize: 20,
     height: 28 / 20,
-    fontFamily: 'Poppins-SemiBold',
     color: ColorLxp.neutral800,
   );
 
   static const TextStyle titleContentBlank = TextStyle(
     fontSize: 20,
     height: 28 / 20,
-    fontFamily: 'Poppins-SemiBold',
     color: ColorLxp.neutral400,
   );
 
   static const TextStyle textButtonBlank = TextStyle(
     fontSize: 16,
-    height: 24 / 16,
-    fontFamily: 'Poppins-Medium',
+    height: 24 / 12,
     color: ColorLxp.white,
   );
 
   static const TextStyle textTitleCourse = TextStyle(
-    fontFamily: 'Poppins-SemiBold',
     fontSize: 14,
     height: 21 / 14,
     color: ColorLxp.neutral800,
   );
 
   static const TextStyle textSks = TextStyle(
-    fontFamily: 'Poppins-Regular',
     fontSize: 12,
     height: 18 / 12,
     color: ColorLxp.neutral500,
   );
 
   static const TextStyle textIndicator = TextStyle(
-    fontFamily: 'Poppins_Regular',
     fontSize: 10,
     height: 18 / 10,
     color: ColorLxp.neutral500,
   );
-
-  // static const TextStyle label = TextStyle(
-  //   fontSize: 12,
-  //   height: 18 / 12,
-  //   fontFamily: 'Poppins-Medium',
-  //   color: ColorLxp.neutral300,
-  // );
 }
