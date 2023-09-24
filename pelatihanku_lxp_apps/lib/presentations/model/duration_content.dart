fb4700b938f9af2a638d290c3c64358077d499b6
raihan <raihanp0308@gmail.com>
2023-09-24 13:36:43 +0700

    add models, first list content and etc

---
+++ pelatihanku_lxp_apps/lib/presentations/model/duration_content.dart
@@ -0,0 +1,6 @@
+List<String> durationContent = [
+  '8 Menit',
+  '8 Menit',
+  '2 Hari',
+  '1 Minggu',
+];
--- pelatihanku_lxp_apps/lib/presentations/screens/detail_pelatihanku.dart
+++ pelatihanku_lxp_apps/lib/presentations/screens/detail_pelatihanku.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pelatihanku_lxp_apps/presentations/commons/checkbox_lxp.dart';
 import 'package:pelatihanku_lxp_apps/presentations/utils/color.dart';
 import 'package:pelatihanku_lxp_apps/presentations/utils/style.dart';
+import 'package:pelatihanku_lxp_apps/presentations/views/first_list_content.dart';
 
 class DetailPelatihanku extends StatefulWidget {
   const DetailPelatihanku({Key? key}) : super(key: key);
@@ -13,15 +14,7 @@ class DetailPelatihanku extends StatefulWidget {
 
 class _DetailPelatihankuState extends State<DetailPelatihanku> {
   bool isClicked = false;
-  bool isContentVisible1 = false;
   bool isContentVisible2 = false;
-
-  List<String> titleContent1 = [
-    'Pengenalan 1',
-    'Keterampilan Komunikasi 1',
-    'Tugas - Pertemuan 1',
-    'Quiz - Pertemuan 1',
-  ];
 
   List<String> titleContent2 = [
     'Pengenalan 2',
@@ -90,144 +83,7 @@ class _DetailPelatihankuState extends State<DetailPelatihanku> {
               ],
             ),
             const SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isContentVisible1 = !isContentVisible1;
-                    });
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Pertemuan 1 - Pengenalan',
-                            style: Style.textTitleCourse.copyWith(
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                '0/2',
-                                style: Style.textSks
-                                    .copyWith(color: ColorLxp.neutral800),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 5.0),
-                                child: Text(
-                                  '|',
-                                  style: Style.textSks
-                                      .copyWith(color: ColorLxp.neutral800),
-                                ),
-                              ),
-                              const Icon(
-                                Icons.alarm,
-                                size: 18,
-                                color: ColorLxp.neutral800,
-                              ),
-                              const SizedBox(width: 4),
-                              Text(
-                                '16 Mnt',
-                                style: Style.textSks.copyWith(
-                                  fontWeight: FontWeight.w400,
-                                  color: ColorLxp.neutral800,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        child: isContentVisible1
-                            ? Transform.rotate(
-                                angle: -math.pi / 2,
-                                child: const Icon(
-                                  Icons.chevron_right,
-                                  color: ColorLxp.neutral800,
-                                ),
-                              )
-                            : Transform.rotate(
-                                angle: math.pi / 2,
-                                child: const Icon(
-                                  Icons.chevron_right,
-                                  color: ColorLxp.neutral800,
-                                ),
-                              ),
-                      ),
-                    ],
-                  ),
-                ),
-                Visibility(
-                  visible: isContentVisible1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: List.generate(
-                        titleContent1.length,
-                        (index) {
-                          return Row(
-                            children: [
-                              Text(
-                                (index + 1).toString(),
-                                style: Style.textButtonBlank.copyWith(
-                                  color: ColorLxp.neutral800,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              const SizedBox(width: 12),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    titleContent1[index],
-                                    style: Style.textButtonBlank.copyWith(
-                                      color: ColorLxp.neutral500,
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 15,
-                                    ),
-                                  ),
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.alarm,
-                                        size: 18,
-                                        color: ColorLxp.neutral500,
-                                      ),
-                                      const SizedBox(width: 4),
-                                      Text(
-                                        durationContent[index],
-                                        style: Style.textSks.copyWith(
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                              const Spacer(),
-                              index == 3
-                                  ? const Icon(
-                                      Icons.lock,
-                                      color: ColorLxp.neutral500,
-                                    )
-                                  : const CheckboxLxp(),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            const FirstListContent(),
             const SizedBox(height: 30), // Spasi antara dua dropdown
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
