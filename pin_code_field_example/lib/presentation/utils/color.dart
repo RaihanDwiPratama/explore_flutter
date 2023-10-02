3ae577e7d3f6600c1574ef167b166612fa84d51c
raihan <raihanp0308@gmail.com>
2023-10-02 17:36:27 +0700

    mecah kode

---
+++ pin_code_field_example/lib/presentation/utils/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorDcoll {
+  static Color grey = const Color(0xff292D32);
+  static Color greyModern = const Color(0xffEEF2F6);
+  static Color grayModern = const Color(0xff9AA4B2);
+}
---
+++ pin_code_field_example/lib/presentation/utils/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class StyleDcoll {
+  static TextStyle title = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle description = const TextStyle(
+    fontSize: 16,
+    color: Color(0xff697586),
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle newPassword = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+}
