e2f6d0d92daa6790509e63442ac8203077abd75c
raihan <raihanp0308@gmail.com>
2023-10-07 17:53:10 +0700

    initial

---
+++ news_app/lib/src/utils/constant/strings.dart
@@ -0,0 +1,10 @@
+// App
+const String appTitle = 'Flutter Clean Architecture';
+
+// Networkking and APIs
+const String baseUrl = 'https://newsapi.org/';
+const String defaultApiKey = '218d910847e74f3989c337ed714e2aca';
+
+// Storage and Databases
+const String articlesTableName = 'articles_table';
+const String databaseName = 'app_database.db';
