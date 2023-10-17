78eeb0a790ede6f28756c48ed1857cf022ebd450
raihan <raihanp0308@gmail.com>
2023-10-17 17:14:51 +0700

    add tugas pertemuan

--- pelatihanku_lxp_apps/lib/presentations/screens/pelatihanku/tugas/detail_tugas.dart
+++ pelatihanku_lxp_apps/lib/presentations/screens/pelatihanku/tugas/detail_tugas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pelatihanku_lxp_apps/presentations/utils/color.dart';
 import 'package:pelatihanku_lxp_apps/presentations/utils/style.dart';
 
@@ -71,6 +72,83 @@ class DetailTugas extends StatelessWidget {
                 )
               ],
             ),
+            Container(
+              height: 235,
+              width: double.infinity,
+              margin: const EdgeInsets.only(
+                top: 16,
+                bottom: 28,
+              ),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: ColorLxp.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    color: ColorLxp.black.withOpacity(0.10),
+                    offset: const Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tugas Pertemuan 1',
+                    style: Style.textTitleCourse.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'Berikan contoh konkret dari kehidupan sehari-hari tentang proses komunikasi yang melibatkan pengirim, pesan, saluran, penerima, dan umpan balik. Jelaskan setiap elemen proses komunikasi ini dalam konteks contoh yang Anda berikan.',
+                    style: Style.textSks.copyWith(
+                      fontWeight: FontWeight.w400,
+                      color: ColorLxp.black,
+                    ),
+                  ),
+                  Container(
+                    height: 36,
+                    padding: const EdgeInsets.all(8),
+                    margin: const EdgeInsets.symmetric(vertical: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: ColorLxp.neutral300,
+                        strokeAlign: 1,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/icons/FilePdf.svg'),
+                        const SizedBox(width: 4),
+                        Text(
+                          'Tugas Komunikasi 1.pdf',
+                          style: Style.textSks.copyWith(
+                            fontWeight: FontWeight.w400,
+                            color: ColorLxp.neutral800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SvgPicture.asset('assets/icons/CalendarCheck.svg'),
+                      const SizedBox(width: 4),
+                      Text(
+                        ' 15/09/2023 - 23:59:00',
+                        style: Style.textSks.copyWith(
+                          fontWeight: FontWeight.w400,
+                          color: ColorLxp.neutral800,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
