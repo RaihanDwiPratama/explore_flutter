30124d078b2989ea1cf4644783e769e074f6a8c8
raihan <raihanp0308@gmail.com>
2023-11-17 14:29:06 +0700

    fix quiz widget

--- pelatihanku_lxp_apps/lib/presentations/screens/mycourse/quiz/soal_quiz/widgets/quiz.dart
+++ pelatihanku_lxp_apps/lib/presentations/screens/mycourse/quiz/soal_quiz/widgets/quiz.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pelatihanku_lxp_apps/presentations/screens/mycourse/quiz/soal_quiz/widgets/answer.dart';
+import 'package:pelatihanku_lxp_apps/presentations/screens/mycourse/quiz/soal_quiz/widgets/question.dart';
 
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
@@ -14,6 +16,18 @@ class Quiz extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Question],);
+    return Column(
+      children: [
+        Question(
+          questionText: questions[questionIndex]['questionText'].toString(),
+        ),
+        ...(questions[questionIndex]['answer'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+              selectHandler: () => answerQuestion(answer['score']),
+              answerText: answer['text'].toString());
+        }).toList()
+      ],
+    );
   }
 }
