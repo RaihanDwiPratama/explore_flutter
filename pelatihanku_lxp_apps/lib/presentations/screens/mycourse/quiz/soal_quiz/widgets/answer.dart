dd5ecbf85626c6acd1ef610719e4b128db52331d
raihan <raihanp0308@gmail.com>
2023-11-17 14:22:25 +0700

    add answer file

---
+++ pelatihanku_lxp_apps/lib/presentations/screens/mycourse/quiz/soal_quiz/widgets/answer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  const Answer({
+    super.key,
+    required this.selectHandler,
+    required this.answerText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: selectHandler(),
+        child: Text(answerText),
+      ),
+    );
+  }
+}
